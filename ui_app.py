ea701c24e27e2b68dfe71d04ef4c13b8ecf97ed2
Coro-sensei <acoro1719@gmail.com>
2025-06-26 18:42:06 +0800

    wrote the def functions

--- ui_app.py
+++ ui_app.py
@@ -8,4 +8,17 @@ class CalculatorApp:
         self.root = root
         self.root.title("Calculator")
 
-        
+        self.logic = CalculatorLogic()
+        self.result_var = tk.StringVar
+
+        self.create_widgets()
+        self.setup_grid()
+        self.bind_keys()
+
+        self.root.resizable(False, False)
+
+    def create_widgets(self):
+    
+    def setup_grid(self):
+    
+    def bind_keys(self):
